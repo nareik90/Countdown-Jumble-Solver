bb45b4495cd046ac8158d2185f5fac8a67bac53c
kieran <G00245082:gmit.ie>
2016-02-15 19:50:17 +0000

    Tried a refactorial method

--- mainCountdown.py
+++ mainCountdown.py
@@ -5,19 +5,30 @@ import random
 
 print ("Are you ready to play?")
 
+def factorial(n):
+    return n * factorial(n-1)
+
 def id_generator(size=9, chars=string.ascii_uppercase):
     return ''.join(random.SystemRandom().choice(string.ascii_uppercase) for _ in range(size))
 
 s = id_generator()    
 
-print (s)
+"""print (s)"""
 
+while factorial(9):
+    if s not in list:
+        continue
+    else:
+        s = (random.sample(word, len(word)))
+        list = []
+        list.append(s)
 
-f = open('wordlist.txt', 'r')
-for line in f:
-    s =  ''.join(random.sample(s, len(s)))
-    for word in line.split():
-        if word not in s:
-            continue
-        else: 
-            print(word)
+print (list)
+
+with open('wordlist.txt','r') as f:
+    for line in f:
+        for word in line.split():
+            if word not in s:
+                continue
+            else: 
+                print(word)
