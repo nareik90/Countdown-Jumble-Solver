adad02f0f60e634550e4d88635970e6ab256e249
kieran <G00245082:gmit.ie>
2016-02-26 15:26:08 +0000

    Update

--- mainCountdown.py
+++ mainCountdown.py
@@ -1,21 +1,38 @@
+from random import randint
+import time
 import string
-import random
+
 
 """with open('wordlist.txt','r') as f:"""
+f = open('wordList.txt', 'r')
+v = open ('vouls.txt', 'r')
+c= open ('cons.txt', 'r')
+jumbleSort = ' '
+jumble = ''
 
-print ("Are you ready to play?")
+def jumbler():
+    wordsList = []
+    for i in range(0, 4):
+        voul = randint(0, 4)
+        v = open ('vouls.txt')
+        lines=v.readlines()
+        word = lines[voul]
+        wordsList.insert(i, word)
+     
+    conWordList = []
+    for x in range(5, 10):
+        con = randint(0, 15)
+        c = open ('cons.txt')
+        conLines=c.readlines()
+        word = conLines[con]
+        wordsList.insert(x, word)
 
-def id_generator(size=9, chars=string.ascii_uppercase):
-    return ''.join(random.SystemRandom().choice(string.ascii_uppercase) for _ in range(size))
-
-
-s = id_generator()    
-"""jumbleSort = ''.join(sorted(s))"""
-jumbleSort = ''.join(sorted("abalienation"))
-
-print (s)
-print (jumbleSort)
-
+    wordJumble = ''.join(wordsList)  
+    #print(wordJumble)
+    wordJumble = ''.join(sorted(wordJumble))
+    print(wordJumble)
+    return wordJumble
+##print (jumbleSort)
 
 def compare(ch, text): 
     numAppears = 0
@@ -24,12 +41,44 @@ def compare(ch, text):
             numAppears += 1
     return numAppears
 
-with open('wordlist.txt','r') as f:
+def solved(jumbleSort):
+
+    solution = []
     for line in f:
         for word in line.split():
-            dic = ''.join(sorted(word))
-            matches = compare(dic, jumbleSort);
-            if matches > 3:
-                print (word)
+            if word in jumbleSort:
+            	if(len(word) > 2):
+                    solution.append(word)
+                    print('checking.....')
             else:
-            	continue
+        	    continue
+    return solution
+
+start_time = time.time()
+
+#anagrams = solved(jumbleSort)
+
+##print("Result:", anagrams)
+##print("Solved in %s seconds" % (time.time() - start_time))
+
+userInput = ''
+temprompt = None
+
+if (temprompt == None):
+
+    var = input("Would You like to jumble some letters, Yes?: ")
+
+    if ('yes' in var):
+        mixer = jumbler()
+        done = solved(mixer)
+        print(done)
+        temprompt
+        print("Solved in %s seconds" % (time.time() - start_time))
+
+if(temprompt):
+    if ('If you already have the letters lets solve it: Yes' in var):
+        solved(mixer)
+
+    #jumble = jumbler()
+
+    #jumbleSort = ''.join(sorted(jumble))
