34fdaa230187a226be4df8e722cbc75da9e576d8
kieran <G00245082:gmit.ie>
2016-03-02 21:15:08 +0000

    Player can now select the letters

--- mainCountdown.py
+++ mainCountdown.py
@@ -9,11 +9,12 @@ f = open('wordList.txt', 'r')
 v = open ('vouls.txt', 'r')
 c= open ('cons.txt', 'r')
 
+wordList = []
+
 # This method takes 4 random strings from a .txt file containing all the vouls
 # and 5 random strings from a .txt file containg constanents. The strings are joined and organised 
 # alphabetically.
 def jumbler():
-    wordsList = []
     for i in range(0, 4):
         voul = randint(0, 4)
         v = open ('vouls.txt')
@@ -23,17 +24,35 @@ def jumbler():
      
     conWordList = []
     for x in range(5, 10):
-        con = randint(0, 15)
+        con = randint(0, 19)
         c = open ('cons.txt')
         conLines=c.readlines()
         word = conLines[con]
         wordsList.insert(x, word)
 
-    wordJumble = ''.join(wordsList)  
+    wordJumble = ''.join(wordList)  
     #print(wordJumble)
     wordJumble = ''.join(sorted(wordJumble))
     print(wordJumble)
     return wordJumble
+
+def getVoul():
+    voul = randint(0, 4)
+    v = open ('vouls.txt')
+    lines=v.readlines()
+    word = lines[voul]
+    wordList.append(word)
+    wordJumble = ''.join(wordList)  
+    return 
+
+def getConstenent():
+    con = randint(0, 19)
+    c = open ('cons.txt')
+    conLines=c.readlines()
+    word = conLines[con]
+    wordList.append(word)
+    wordJumble = ''.join(wordList)  
+    return 
 
 #This method was to originaly compare the unorganised strings but decided to go with 
 #the solved method instead.
@@ -53,30 +72,53 @@ def solved(jumbleSort):
             sort = ''.join(sorted(word))
             if sort in jumbleSort:
             	if(len(word) > 2):
-                    solution.append(word)
-                   
+                    solution.append(word)    
             else:
         	    continue
     return solution
 
-start_time = time.time()
-
 
 temprompt = None
+selected = None
+voulCount = 0
+conCount = 0
 
-if (temprompt == None):
-
-    var = input("Would You like to jumble some letters, Yes?: ")
-
-    if ('yes' in var):
-        mixer = jumbler()
-        done = solved(mixer)
+while (temprompt == None): 
+    var = input("What would you like to do? Select or Solve: ")
+  #  if ('Jumble' in var): 
+   #     mixer = jumbler() 
+    #    done = solved(mixer)
+     #   solved(mixer)   
+      #  selected
+    if('Solve' in var):
+        start_time = time.time()
+        done = solved(wordJumble)
         print('All possible solutions')
         print(done)
-        temprompt
         print("Solved in %s seconds" % (time.time() - start_time))
+        temprompt
+        wordJumble = " "
 
-if(temprompt):
-    if ('If you already have the letters lets solve it: Yes' in var):
-        solved(mixer)
+    while(selected == None):
 
+        if('Select' in var):
+            letter = input("Would you like a voul(V) or constanent (C)")
+
+        if('V' in letter):
+            if(voulCount < 4):
+                getVoul()
+                voulCount += 1
+                wordJumble = ''.join(wordList)  
+                wordJumble = ''.join(sorted(wordJumble))
+                print(wordJumble)
+            else: selected = True
+
+        if('C' in letter, conCount < 5):
+            if(conCount < 5):
+                getConstenent()
+                conCount += 1
+                wordJumble = ''.join(wordList)  
+                wordJumble = ''.join(sorted(wordJumble))
+                print(wordJumble)
+            else: selected = True
+    
